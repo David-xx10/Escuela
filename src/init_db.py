4a5df1a1c35e2959ac85c6e4b82bd0eac2f165a2
David-xx10 <castaneda.2906.david@gmail.com>
2026-09-13 01:19:22 -0500

    feat: confidurar-main-orm

--- src/init_db.py
+++ src/init_db.py
@@ -8,6 +8,9 @@ from src.entities.periodo_academico import PeriodoAcademico
 from src.entities.profesor import Profesor
 from src.entities.estudiante import Estudiante
 from src.entities.nota import Nota
+from src.entities.evaluacion import Evaluacion
+from src.entities.matricula import Matricula
+from src.entities.persona import Persona
 
 print("Conectando a Neon y creando tablas...")
 Base.metadata.create_all(bind=engine)
